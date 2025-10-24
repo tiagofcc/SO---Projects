4ae5730ef3f31c0214dbd226d87770cff3bbbd00
Tiago Costa <tiagofccosta@ua.pt>
2025-10-24 17:36:50 +0100

    initialize_bin

--- Projeto_01/RecycleByn.sh
+++ Projeto_01/RecycleByn.sh
@@ -11,7 +11,8 @@
 RECYCLE_BIN_DIR="$HOME/.recycle_bin" 
 FILES_DIR="$RECYCLE_BIN_DIR/files" 
 METADATA_FILE="$RECYCLE_BIN_DIR/metadata.db" 
-CONFIG_FILE="$RECYCLE_BIN_DIR/config" 
+CONFIG_FILE="$RECYCLE_BIN_DIR/config"
+LOG_FILE="$RECYCLE_BIN_DIR/recycle.log"
  
 # Color codes for output (optional) 
 RED='\033[0;31m' 
@@ -27,11 +28,24 @@ NC='\033[0m' # No Color
 ################################################# 
 initialize_recyclebin() { 
     if [ ! -d "$RECYCLE_BIN_DIR" ]; then 
-        mkdir -p "$FILES_DIR" 
+        mkdir -p "$FILES_DIR"
+
+
         touch "$METADATA_FILE" 
         echo "# Recycle Bin Metadata" > "$METADATA_FILE" 
-        echo "ID,ORIGINAL_NAME,ORIGINAL_PATH,DELE-
-TION_DATE,FILE_SIZE,FILE_TYPE,PERMISSIONS,OWNER" >> "$METADATA_FILE" 
+        echo "ID,ORIGINAL_NAME,ORIGINAL_PATH,DELETION_DATE,FILE_SIZE,FILE_TYPE,PERMISSIONS,OWNER" >> "$METADATA_FILE"
+
+        touch "$CONFIG_FILE"    #criar um config file caso este não exista
+        echo "# Recycle Bin Configuration" > "$CONFIG_FILE"     #dar um cabeçalho tal como no metadata
+        echo 'RECYCLE_BIN_DIR="$HOME/.recycle_bin"' >> "$CONFIG_FILE"    #adicionar ao config o caminho do diretorio do recicle bin
+        echo 'FILES_DIR="$RECYCLE_BIN_DIR/files"' >> "$CONFIG_FILE"    #adicionar ao config o caminho do diretorio files
+        echo 'METADATA_FILE="$RECYCLE_BIN_DIR/metadata.db"' >> "$CONFIG_FILE"    #adicionar ao config o caminho do ficheiro metadata
+        echo "MAX_SIZE_MB=1024" >> "$CONFIG_FILE"   #adicionar ao config o tamaho maximo do ficheiros que podem ir para o recycle bin
+        echo "RETENTION_DAYS=30" >> "$CONFIG_FILE"  #adicionar o tempo limite em que um ficheiro pode ficar dentro do recycle bin
+        echo 'CONFIG_FILE="$RECYCLE_BIN_DIR/config"' >> "$CONFIG_FILE" #adicionar ao config o seu proprio config
+
+        touch "$LOG_FILE" #cria o ficheiro recyclebin.log vazio caso este não exista
+
         echo -e "${GREEN}Recycle bin initialized at $RECYCLE_BIN_DIR${NC}" #Alteração da função original para a mensagem de sucesso ser verde
         return 0 
     fi 
--- RecycleBin_test.sh
+++ RecycleBin_test.sh
@@ -14,15 +14,28 @@ CONFIG_FILE="$RECYCLE_BIN_DIR/config"
 ################################################# 
 initialize_recyclebin() { 
     if [ ! -d "$RECYCLE_BIN_DIR" ]; then 
-        mkdir -p "$FILES_DIR" 
+        mkdir -p "$FILES_DIR"
+
+
         touch "$METADATA_FILE" 
         echo "# Recycle Bin Metadata" > "$METADATA_FILE" 
-        echo "ID,ORIGINAL_NAME,ORIGINAL_PATH,DELETION_DATE,FILE_SIZE,FILE_TYPE,PERMISSIONS,OWNER" >> "$METADATA_FILE" 
-        echo "Recycle bin initialized at $RECYCLE_BIN_DIR" 
+        echo "ID,ORIGINAL_NAME,ORIGINAL_PATH,DELETION_DATE,FILE_SIZE,FILE_TYPE,PERMISSIONS,OWNER" >> "$METADATA_FILE"
+
+        touch "$CONFIG_FILE"    #criar um conig file caso este não exista
+        echo "# Recycle Bin Configuration" > "$CONFIG_FILE"     #dar um cabeçalho tal como no metadata
+        echo "RECYCLE_BIN_DIR="$HOME/.recycle_bin"" >> "$CONFIG_FILE"    #adicionar ao config o caminho do diretorio do recicle bin
+        echo "FILES_DIR="$RECYCLE_BIN_DIR/files"" >> "$CONFIG_FILE"    #adicionar ao config o caminho do diretorio files
+        echo "METADATA_FILE="$RECYCLE_BIN_DIR/metadata.db"" >> "$CONFIG_FILE"    #adicionar ao config o caminho do ficheiro metadata
+        echo "MAX_SIZE_MB=1024" >> "$CONFIG_FILE"   #adicionar ao config o tamaho maximo do ficheiros que podem ir para o recycle bin
+        echo "RETENTION_DAYS=30" >> "$CONFIG_FILE"  #adicionar o tempo limite em que um ficheiro pode ficar dentro do recycle bin
+        echo "CONFIG_FILE="$RECYCLE_BIN_DIR/config"" >> "$CONFIG_FILE" #adicionar ao config o seu proprio config
+
+        touch "$
+        echo -e "${GREEN}Recycle bin initialized at $RECYCLE_BIN_DIR${NC}" #Alteração da função original para a mensagem de sucesso ser verde
         return 0 
     fi 
     return 0 
-}
+} 
 
 
 main() { 
